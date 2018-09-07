9c89563447cae2fdf8217001c86a0ee9c54874ad
Simon Fernandez <sfernandez31@gatech.edu>
2018-09-07 10:41:04 -0500

    Implemented calc in Person4.

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -30,8 +30,11 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      StringBuilder output = new StringBuilder();
+      for (int i = 0; i < input.length(); i++) {
+        output.append(input.charAt(i) + 1);
+      }
+      return output.toString();
     }
     
     /**
